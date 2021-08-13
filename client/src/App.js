a8caade389b1c7d7c6326b5ababf0f8e10bd2f66
JJ Gray <jjgray@scottlogic.com>
2021-08-13 09:55:20 +0100

    working chess

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,75 @@ import React, { useState } from "react";
 import { Board } from "./components/Board";
 
 export function App() {
-  const [tiles, setTiles] = useState(new Array(...initialPosition));
+  const initialPosition = [
+    ["♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜"],
+    ["♟︎", "♟︎", "♟︎", "♟︎", "♟︎", "♟︎", "♟︎", "♟︎"],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["♙", "♙", "♙", "♙", "♙", "♙", "♙", "♙"],
+    ["♖", "♘", "♗", "♕", "♔", "♗", "♘", "♖"],
+  ];
 
-  const [selected, setSelected] = useState(new Array(64).fill(false));
+  const [tiles, setTiles] = useState(initialPosition);
 
-  function handleClick(i) {
-    let newSelected = [...selected];
-    newSelected.splice(i, 1, !newSelected[i]);
-    setSelected(newSelected);
-  }
+  const [selected, setSelected] = useState(
+    new Array(8).fill(new Array(8).fill(false))
+  );
+
+  const [selection, setSelection] = useState([-1, -1]);
+
+  const copy2D = (array) => {
+    let result = [];
+    array.forEach((subArray) => {
+      result.push(subArray.slice());
+    });
+    return result;
+  };
+
+  const set2D = (array, row, col, value) => {
+    let arrayCopy = copy2D(array);
+    let newRow = arrayCopy[row];
+    newRow.splice(col, 1, value);
+    arrayCopy.splice(row, 1, newRow);
+    return arrayCopy;
+  };
+
+  const movePiece = (startRow, startCol, endRow, endCol) => {
+    const piece = tiles[startRow][startCol];
+    let newBoard = set2D(tiles, endRow, endCol, piece);
+    newBoard = set2D(newBoard, startRow, startCol, "");
+    setTiles(newBoard);
+  };
+
+  const handleClick = (row, col) => {
+    const pieceAtClick = tiles[row][col];
+    if (selection[0] === -1 && selection[1] === -1 && pieceAtClick) {
+      // Set selection
+      const newSelected = set2D(selected, row, col, true);
+      setSelected(newSelected);
+      setSelection([row, col]);
+      console.log("set selection");
+    } else if (selection[0] !== -1 && selection[1] !== -1) {
+      const pieceAtSelection = tiles[selection[0]][selection[1]];
+      if (selection[0] === row && selection[1] === col) {
+        // Remove selection
+        const newSelected = set2D(selected, row, col, false);
+        setSelected(newSelected);
+        setSelection([-1, -1]);
+        console.log("remove selection");
+      } else if (tiles[selection[0]][selection[1]] !== "") {
+        // Move piece
+        movePiece(selection[0], selection[1], row, col);
+        const newSelected = set2D(selected, selection[0], selection[1], false);
+        setSelected(newSelected);
+        setSelection([-1, -1]);
+        console.log("move piece");
+        console.log(newSelected);
+      }
+    }
+  };
 
   return (
     <div className="game">
@@ -18,87 +78,9 @@ export function App() {
         <Board
           tiles={tiles}
           selected={selected}
-          onClick={(i) => handleClick(i)}
+          onClick={(row, col) => handleClick(row, col)}
         />
       </div>
     </div>
   );
 }
-
-const doubleArray = [
-  ["r", "n", "b", "q", "k", "b", "n", "r"],
-  ["p", "p", "p", "p", "p", "p", "p", "p"],
-  ["", "", "", "", "", "", "", ""],
-  ["", "", "", "", "", "", "", ""],
-  ["", "", "", "", "", "", "", ""],
-  ["", "", "", "", "", "", "", ""],
-  ["P", "P", "P", "P", "P", "P", "P", "P"],
-  ["R", "N", "B", "Q", "K", "B", "N", "R"],
-];
-
-const initialPosition = [
-  "♜",
-  "♞",
-  "♝",
-  "♛",
-  "♚",
-  "♝",
-  "♞",
-  "♜",
-  "♟︎",
-  "♟︎",
-  "♟︎",
-  "♟︎",
-  "♟︎",
-  "♟︎",
-  "♟︎",
-  "♟︎",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "♙",
-  "♙",
-  "♙",
-  "♙",
-  "♙",
-  "♙",
-  "♙",
-  "♙",
-  "♖",
-  "♘",
-  "♗",
-  "♕",
-  "♔",
-  "♗",
-  "♘",
-  "♖",
-];
--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -1,99 +1,29 @@
 import { Tile } from "./Tile";
 
 export function Board(props) {
-  function renderTile(i) {
+  const renderTile = (row, col) => {
     return (
       <Tile
-        value={props.tiles[i]}
-        onClick={() => props.onClick(i)}
-        selected={props.selected[i]}
-        dark={Math.floor(i / 8) % 2 ? (i - 1) % 2 : i % 2}
+        value={props.tiles[row][col]}
+        onClick={() => props.onClick(row, col)}
+        selected={props.selected[row][col]}
+        dark={row % 2 ? (col - 1) % 2 : col % 2}
       />
     );
-  }
+  };
 
-  return (
-    <div>
-      <div className="board-row">
-        {renderTile(0)}
-        {renderTile(1)}
-        {renderTile(2)}
-        {renderTile(3)}
-        {renderTile(4)}
-        {renderTile(5)}
-        {renderTile(6)}
-        {renderTile(7)}
-      </div>
-      <div className="board-row">
-        {renderTile(8)}
-        {renderTile(9)}
-        {renderTile(10)}
-        {renderTile(11)}
-        {renderTile(12)}
-        {renderTile(13)}
-        {renderTile(14)}
-        {renderTile(15)}
-      </div>
-      <div className="board-row">
-        {renderTile(16)}
-        {renderTile(17)}
-        {renderTile(18)}
-        {renderTile(19)}
-        {renderTile(20)}
-        {renderTile(21)}
-        {renderTile(22)}
-        {renderTile(23)}
-      </div>
-      <div className="board-row">
-        {renderTile(24)}
-        {renderTile(25)}
-        {renderTile(26)}
-        {renderTile(27)}
-        {renderTile(28)}
-        {renderTile(29)}
-        {renderTile(30)}
-        {renderTile(31)}
-      </div>
-      <div className="board-row">
-        {renderTile(32)}
-        {renderTile(33)}
-        {renderTile(34)}
-        {renderTile(35)}
-        {renderTile(36)}
-        {renderTile(37)}
-        {renderTile(38)}
-        {renderTile(39)}
-      </div>
-      <div className="board-row">
-        {renderTile(40)}
-        {renderTile(41)}
-        {renderTile(42)}
-        {renderTile(43)}
-        {renderTile(44)}
-        {renderTile(45)}
-        {renderTile(46)}
-        {renderTile(47)}
-      </div>
-      <div className="board-row">
-        {renderTile(48)}
-        {renderTile(49)}
-        {renderTile(50)}
-        {renderTile(51)}
-        {renderTile(52)}
-        {renderTile(53)}
-        {renderTile(54)}
-        {renderTile(55)}
-      </div>
-      <div className="board-row">
-        {renderTile(56)}
-        {renderTile(57)}
-        {renderTile(58)}
-        {renderTile(59)}
-        {renderTile(60)}
-        {renderTile(61)}
-        {renderTile(62)}
-        {renderTile(63)}
-      </div>
-    </div>
-  );
+  const getBoardTiles = () => {
+    let board = [];
+    let rowTiles;
+    for (let row = 0; row < 8; row++) {
+      rowTiles = [];
+      for (let col = 0; col < 8; col++) {
+        rowTiles.push(renderTile(row, col));
+      }
+      board[row] = <div className="board-row">{rowTiles}</div>;
+    }
+    return board;
+  };
+
+  return <div>{getBoardTiles()}</div>;
 }
