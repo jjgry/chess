6b06ab6295cf5f456727c1cea7a526eb7906fa0b
JJ Gray <jjgray@scottlogic.com>
2021-08-17 10:40:00 +0100

    simpler code

--- client/src/components/Page.js
+++ client/src/components/Page.js
@@ -2,59 +2,57 @@ import Game from "../contaniers/Game";
 import styles from "./Page.module.css";
 import logo from "./chess.png";
 
-const Page = () => {
-  return (
-    <div className={styles.page}>
-      <div className={styles.logo}>
-        <img className={styles.logoimg} src={logo} alt="chess logo" />
-      </div>
-      <div className={styles.header}>
-        <h1>JJ's Bad 90s Chess</h1>
-      </div>
-      <div className={styles.content}>
-        <p>
-          Do you like <span className={styles.randomWhiteText}>chess</span>?
-          You'll{" "}
-          <i>
-            <b>love</b>
-          </i>{" "}
-          the game you see below!{" "}
-        </p>
-        <p>
-          Play bad 90s chess today on this web app made with{" "}
-          <a href="https://create-react-app.dev/">create-react-app</a>
-        </p>
-        <Game />
-      </div>
-      <div className={styles.contentleft}>
-        <button onClick={() => window.open("./", "_self")}>Home</button>
-        <button onClick={() => window.open("./about", "_self")}>About</button>
-        <button onClick={() => window.open("./chess", "_self")}>Chess</button>
-        <button onClick={() => window.open("./games", "_self")}>
-          Other cool games
-        </button>
-      </div>
-      <div className={styles.contentright}>
-        <h2>Welcome to my website!</h2>
-        <ul>
-          <li>Check out this awesome game!</li>
-          <li>
-            <img href="./" alt="advert.jpg" />
-            <p>
-              you won't <i>believe</i> this one trick to longer life!
-            </p>
-          </li>
-          <li>
-            Learn chess in only 20 minutes at{" "}
-            <a href="https://example.com/you-suck-at-chess">
-              example.com/you-suck-at-chess
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div className={styles.footer}>© 2021 JJ Gray </div>
+const Page = () => (
+  <div className={styles.page}>
+    <div className={styles.logo}>
+      <img className={styles.logoimg} src={logo} alt="chess logo" />
     </div>
-  );
-};
+    <div className={styles.header}>
+      <h1>JJ's Bad 90s Chess</h1>
+    </div>
+    <div className={styles.content}>
+      <p>
+        Do you like <span className={styles.randomWhiteText}>chess</span>?
+        You'll{" "}
+        <i>
+          <b>love</b>
+        </i>{" "}
+        the game you see below!{" "}
+      </p>
+      <p>
+        Play bad 90s chess today on this web app made with{" "}
+        <a href="https://create-react-app.dev/">create-react-app</a>
+      </p>
+      <Game />
+    </div>
+    <div className={styles.contentleft}>
+      <button onClick={() => window.open("./", "_self")}>Home</button>
+      <button onClick={() => window.open("./about", "_self")}>About</button>
+      <button onClick={() => window.open("./chess", "_self")}>Chess</button>
+      <button onClick={() => window.open("./games", "_self")}>
+        Other cool games
+      </button>
+    </div>
+    <div className={styles.contentright}>
+      <h2>Welcome to my website!</h2>
+      <ul>
+        <li>Check out this awesome game!</li>
+        <li>
+          <img href="./" alt="advert.jpg" />
+          <p>
+            you won't <i>believe</i> this one trick to longer life!
+          </p>
+        </li>
+        <li>
+          Learn chess in only 20 minutes at{" "}
+          <a href="https://example.com/you-suck-at-chess">
+            example.com/you-suck-at-chess
+          </a>
+        </li>
+      </ul>
+    </div>
+    <div className={styles.footer}>© 2021 JJ Gray </div>
+  </div>
+);
 
 export default Page;
