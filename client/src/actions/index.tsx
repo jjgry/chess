59abd13a44650f3863a203f855b50e6acd2c0764
JJ Gray <jjgray@scottlogic.com>
2021-08-17 15:20:20 +0100

    convert actions to ts

--- client/src/actions/index.tsx
+++ client/src/actions/index.tsx
@@ -1,13 +1,13 @@
 import { CLICK_SELECTED, CLICK_UNSELECTED } from "../actiontypes";
 
-export function clickSelected(row, col) {
+export function clickSelected(row: number, col: number) {
   return {
     type: CLICK_SELECTED,
     payload: { row: row, col: col },
   };
 }
 
-export function clickUnselected(row, col) {
+export function clickUnselected(row: number, col: number) {
   return {
     type: CLICK_UNSELECTED,
     payload: { row: row, col: col },
